12a89179b715afe7f2c285f8c050046a0733fdee
y-chan <yuto_tetuota@yahoo.co.jp>
2019-04-10 07:18:13 +0900

    Change Default Address Type

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -108,7 +108,7 @@ enum WalletFeature
 };
 
 //! Default for -addresstype
-constexpr OutputType DEFAULT_ADDRESS_TYPE{OutputType::LEGACY};
+constexpr OutputType DEFAULT_ADDRESS_TYPE{OutputType::P2SH_SEGWIT};
 
 //! Default for -changetype
 constexpr OutputType DEFAULT_CHANGE_TYPE{OutputType::CHANGE_AUTO};

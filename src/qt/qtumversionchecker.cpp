21e0b537c42f08bbbcee9aeb83338b11ddd7d1b6
y-chan <34832037+y-chan@users.noreply.github.com>
2019-04-11 07:40:19 +0900

    fix paternVersion

--- src/qt/qtumversionchecker.cpp
+++ src/qt/qtumversionchecker.cpp
@@ -8,7 +8,7 @@
 #include <QRegularExpression>
 #include <QRegularExpressionMatchIterator>
 
-#define paternVersion "Qtum-([0-9]+\\.)?([0-9]+\\.)?([0-9]+)-"
+#define paternVersion "VIPSTARCOIN-([0-9]+\\.)?([0-9]+\\.)?([0-9]+)-"
 
 QtumVersionChecker::QtumVersionChecker(QObject *parent) : QObject(parent)
 {

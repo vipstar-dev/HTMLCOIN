379330f6a44033d1b2cd121739a07af3b497758e
y-chan <y-chan@y-chan.dev>
2021-04-02 16:33:22 +0900

    change CLIENT_NAME

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -12,7 +12,7 @@
  * for both bitcoind and bitcoin-qt, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("VIPSTARCOIN");
+const std::string CLIENT_NAME("Unofficial VIPS");
 
 /**
  * Client version number
